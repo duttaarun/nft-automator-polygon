b4813b3ca302257d0e864c3fc6d4770eae2a2892
duttaarun <duttaarun2015@gmail.com>
2022-05-28 00:36:26 +0100

    Command args added

--- nft-parse/exec.js
+++ nft-parse/exec.js
@@ -1,5 +1,10 @@
 const { exec } = require("child_process");
-exec("java -jar ./ipfsparse.jar https://gateway.pinata.cloud/ipfs/Q ./template_ipfs.xlsx", (error, stdout, stderr) => {
+const args = process.argv.slice(2);
+
+if(!args[0])
+    throw new Error("You need to specify the folder hash");
+
+exec("java -jar ./ipfsparse.jar "+args[0]+" ./template_ipfs.xlsx", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
